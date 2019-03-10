2080c84ff2594263c91a45cdeed496d8dae25b95
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2019-03-10 13:20:22 +0300

    add AssemblyInfo.cs

---
+++ LogSender/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyProduct("LogSender")]
+[assembly: ComVisible(false)]
+[assembly: Guid("51e2e1ca-72d4-4d2c-a96f-be618762a946")]
+[assembly: AssemblyVersion("1.0.0.0")]
